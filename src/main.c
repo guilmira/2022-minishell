fbfb0f3c1957c16a05be2f7566b617ce0007d86e
EarlyObject <sr.ardak@gmail.com>
2022-02-18 13:34:35 +0100

    changes in main put in place

--- src/main.c
+++ src/main.c
@@ -12,12 +12,12 @@
 
 #include "../include/minishell.h"
 
-//ejecutar comandos en ruta absoluta. ejecutar antes de buscar nada.
-
 /** PURPOSE : Store variables in struct that will be kept after loop. */
 static void	store_program(t_prog *prog, t_arguments *args)
 {
-	prog->envp = args->envp;
+	if (prog->envp)
+		ft_free_split(prog->envp);
+	prog->envp = copy_array(prog->envp, args->envp);
 	prog->status = args->status;
 	prog->builtin_str = args->builtin_str;
 }
@@ -31,6 +31,7 @@ int
 	t_prog		*prog;
 	t_arguments	*arguments;
 	char		*builtin_str[9];
+	int			ret;
 
 	prog = NULL;
 	arguments = NULL;
@@ -42,25 +43,24 @@ int
 		shell_reader(envp, arguments);
 		if (arguments->flag_execution)
 			if (!msh_execute(arguments->argv, arguments))
-			{
-				free_heap_memory(arguments);
 				break ;
-			}
 		store_program(prog, arguments);
+		ft_free_split(arguments->envp);
 		free_heap_memory(arguments);
-		if (0) //temporal
-			break ;
 	}
+	ft_free_split(prog->envp);
 	free(prog); //is not freed in free_heap_memory
-	return (arguments->status);
+	ft_free_split(arguments->envp);
+	ret = arguments->status;
+	free_pointers(1, arguments);
+	return (ret);
 }
 
 //PROVISIONAL -- comment if compiling with fsanitize
-/*  void	*ft_leaks(void)
+void	ft_leaks(void)
 {
 	system("leaks minishell");
-	return (NULL);
-} */
+}
 
 /** EXECUTION : ./minishell
  * This program will run a student made version of the bash console.
@@ -69,11 +69,16 @@ int	main(int argc, char *argv[] __attribute__((unused)), char *envp[])
 {
 	signal(SIGQUIT, SIG_IGN);
 	signal(SIGINT, sig_handler);
-	//atexit(ft_leaks()); //on exit, gves seg fault.
-	 if (argc != ARG_NUMBER)
+	//atexit(ft_leaks);
+	if (argc != ARG_NUMBER)
 		ft_shut(INVALID_ARGC, 0);
 	return (shell_loop(envp));
 }
 
 //https://datacarpentry.org/shell-genomics/04-redirection/index.html
 
+//TODO ctrl + c complexity when several levels of ms. it has to close. it can work with PID
+
+//he<ls|wc<"jsdhghjsjkgk"""<<whatever>>final test
+
+//missign to correct echo "" (take nothing on origin)
