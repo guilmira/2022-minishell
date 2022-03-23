1623ca22cc01f33ec01cd2f08162da1c07b3b0d2
EarlyObject <sr.ardak@gmail.com>
2022-03-23 23:35:46 +0100

    temporal fix of piped heredoc

--- process/2son.c
+++ process/2son.c
@@ -51,11 +51,12 @@ int	last_son(int index, t_arguments *args)
 	command_struct = ft_lst_position(args->commands_lst, args->command_number);
 	if (!command_struct || !command_struct->command)
 		ft_shutdown(LST, 0, args);
-	ret = get_builtins_ret(args, command_struct);
+	ret = get_builtins_ret(args, command_struct); // THE PROBLEM IS HERE
 	if (dup2(args->fds[index], STDIN_FILENO) == -1)
 		ft_shutdown(DUP_ERROR, 0, args);
 	close(args->fds[index]);
 	command_file_setup(command_struct, args);
+	ret = -1; //TEMPORAL LINE
 	if (ret >= 0)
 		return (ret);
 	return (do_execve(args, command_struct));
